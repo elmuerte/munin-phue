605175a787b61735e88c7d54cc4eb242803c6646
Michiel Hendriks <github.com@elmuerte.com>
2014-05-01 22:23:35 +0200

    Cleanup, put more logic into methods. Still not parsing the munin alert input

--- munin-alert-phue.py
+++ munin-alert-phue.py
@@ -5,72 +5,121 @@
 munin-alert-phue.py provides extreme feedback functionality of munin alerts to a Philips Hue system.
 """
 
-import os, argparse, ConfigParser, json
+import logging, os, argparse, ConfigParser, json, time
 from phue import Bridge
 
-debug=False
 
-cmdline = argparse.ArgumentParser(description='Use a Philips Hue system for munin alerting.')
-cmdline.add_argument('-c', '--config', type=argparse.FileType('r'), 
-                     metavar='FILE', help="Read the configuration from FILE.")
-cmdline.add_argument('-s', '--section', default='default', 
-                     metavar='SECTION', help="Use SECTION from the configuration file.")
-cmdline.add_argument('--register',
-                     metavar='HOSTNAME', help="Request a username from the Bridge at HOSTNAME. "
-                     "Perform registration within 30 seconds after pressing the connect button on the bridge. "
-                     "The created username is written to the configuration file.")
-
-args = cmdline.parse_args()
-
-config = ConfigParser.ConfigParser()
-if (args.config == None):
-    config.read(os.path.expanduser('~/.munin-alert-phue.ini'))
-else:
-    config.readfp(args.config)
-
-bridge_host = config.get(args.section, 'hostname')
-bridge_user = config.get(args.section, 'username')
-
-if (bridge_host == None):
-    raise Exception('hostname not defined in config section '+args.section)
-
-if (bridge_user == None):
-    raise Exception('username not defined in config section '+args.section+
-                    '. Start with --register argument to generate a username')
+LEVEL_NORMAL=0
+LEVEL_WARNING=1
+LEVEL_CRITICAL=2
 
 
+def parse_args():
+    """
+    Parse the commandline arguments
+    """
+    cmdline = argparse.ArgumentParser(description='Use a Philips Hue system for munin alerting.')
+    cmdline.add_argument('-c', '--config', type=argparse.FileType('r'), 
+                         metavar='FILE', help="Read the configuration from FILE.")
+    cmdline.add_argument('-s', '--section', default='default', 
+                         metavar='SECTION', help="Use SECTION from the configuration file.")
+    cmdline.add_argument('--register',
+                         metavar='HOSTNAME', help="Request a username from the Bridge at HOSTNAME. "
+                         "Perform registration within 30 seconds after pressing the connect button on the bridge. "
+                         "The created username is written to the configuration file.")
+    return cmdline.parse_args()
 
-# State processing
+
+def load_config(fp):
+    """
+    Load the configuration file
+    """
+    config = ConfigParser.ConfigParser()
+    if (fp == None):
+        log.info('Reading config from ~/.munin-alert-phue.ini')
+        config.read(os.path.expanduser('~/.munin-alert-phue.ini'))
+    else:
+        log.info('Reading config from %s', fp.name)
+        config.readfp(fp)
+    return config
+
 
 def load_state(filename):
-    if os.access(filename, os.R_OK):
-        stateFile = open(filename, 'r')
-        return json.load(stateFile)
-    else:
-        return json.loads('{"current_status":"normal"}')
+    """
+    Load the previous munin alerting state
+    """
+    log.info('Loading state from %s', filename)
+    try:
+        if os.access(filename, os.R_OK):
+            stateFile = open(filename, 'r')
+            return json.load(stateFile)
+    except:
+        pass
+    return {'current_status': LEVEL_NORMAL, 'last_change': time.time()}
+
 
 def save_state(filename, state):
+    """
+    Save the provided munin alerting state
+    """
+    log.info('Saving state to %s', filename)
     stateFile = open(filename, 'w')
     json.dump(state, stateFile)
 
 
-if (config.get(args.section, 'state_file') == None):
-    stateFilename = '~/.munin-alert-phue.'+key+'.db'
-else:
-    stateFilename = config.get(args.section, 'state_file')
-
-stateFilename = os.path.expanduser(stateFilename)
-state = load_state(stateFilename)
-old_status = state['current_status']
-
-# Process status update
-
-save_state(stateFilename,state)
+def update_lights(bridge_host, bridge_user):
+    """
+    Update the lights with the new state
+    """
+    bridge = Bridge(ip=bridge_host, username=bridge_user)
+    # TODO
 
 
+def register(bridge_host):
+    """
+    Register for a user at the provided host
+    """
+    # TODO
 
-# Update the lights when needed
-#bridge = Bridge(ip=bridge_host, username=bridge_user)
 
-# ....
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(levelname)s [%(name)s]: %(message)s', level=logging.DEBUG)
+    log = logging.getLogger('munin.alert.phue')
+
+    args = parse_args()
+
+    if (args.register != None):
+        register(args.register)
+        sys.exit()
+
+    config = load_config(args.config)
+
+
+    bridge_host = config.get(args.section, 'hostname')
+    bridge_user = config.get(args.section, 'username')
+
+    if (bridge_host == None):
+        log.fatal('hostname not defined in config section [%s].', args.section)
+        sys.exit(1)
+
+    if (bridge_user == None):
+        log.fatal('username not defined in config section [%s]. Start with --register argument to generate a username', args.section)
+        sys.exit(1)
+
+
+    if (config.get(args.section, 'state_file') == None):
+        stateFilename = '~/.munin-alert-phue.'+key+'.db'
+    else:
+        stateFilename = config.get(args.section, 'state_file')
+
+    stateFilename = os.path.expanduser(stateFilename)
+    state = load_state(stateFilename)
+    old_status = state['current_status']
+
+    # Process status update
+
+    save_state(stateFilename,state)
+
+
 
