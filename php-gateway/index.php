a5ca2277c4fca35ed6c60ae277968c2c37299e02
Michiel Hendriks <github.com@elmuerte.com>
2014-05-10 22:11:26 +0200

    license text update

--- php-gateway/index.php
+++ php-gateway/index.php
@@ -4,10 +4,22 @@
  * This is a PHP gateway for munin-alert-phue. It will allow you do forward a munin alert
  * from a different network to the network where the lights should be controlled.
  *
- * Copyright 2014 Michiel Hendriks <elmuerte@drunknsipers.com>
- *
- * Released under the terms of GNU Public License version 3 or later
+ * Munin Alert Phue - Munin alerts updating Philips Hue lights.
  * https://github.com/elmuerte/munin-phue 
+ * Copyright (C) 2014 Michiel Hendriks <elmuerte@drunksnipers.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 //Reject any non-put request
